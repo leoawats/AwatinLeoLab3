9bad27035c32bfb38d2a7af48cf57f328fcdd845
leoawats <2015081427@ust-ics.mygbiz.com>
2018-09-13 22:55:36 +0800

    Initial commit

---
+++ app/src/main/java/com/awatin/leo/AwatinLeoLab3.java
@@ -0,0 +1,13 @@
+package com.awatin.leo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AwatinLeoLab3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.awatinleolab3);
+    }
+}
